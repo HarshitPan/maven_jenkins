79f8051b758a859ca5f4a53e6883efbc421c7025
harsh <harshitpandeyfeelings@gmail.com>
2023-01-31 14:27:22 +0530

    first

---
+++ src/main/java/pkg1/NewClass.java
@@ -0,0 +1,14 @@
+package pkg1;
+
+public class NewClass {
+	public void show()
+	{
+		System.out.println("This is a Maven Project...");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		NewClass obj=new NewClass();
+		obj.show();
+	}
+
+}
